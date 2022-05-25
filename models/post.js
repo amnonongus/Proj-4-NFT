aa6cd170b08499e8add5afc5f1736362488973af
amnonongus <charmilano@gmail.com>
2022-05-25 10:24:40 -0700

    update models

--- models/post.js
+++ models/post.js
@@ -6,12 +6,19 @@ const likesSchema = mongoose.Schema({
 })
 
 // A post has many likes, a like belongs to a POST
-const postSchema = new mongoose.Schema({
+const nftSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}, // referencing a model
     photoUrl: String,
     caption: String,
+    name: String,
+    FloorPrice: Number,
+    Volume: Number,
+    Volume24hours: Number,
+    Volume7days: Number,
+    UniqueOwners: Number,
+    Supply: Number,
     likes: [likesSchema] // embedded schema
   })
  
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Post', nftSchema);
